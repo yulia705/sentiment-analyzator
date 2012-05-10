9d19ef9a0d206b67d5c1642f264a9e3be05c54c0
lesha@kalmykovs.com <lesha@kalmykovs.com@4b2eca94-256c-a624-0d39-11434cdaca9f>
2012-05-10 05:31:11 +0000

    added getting marks from reviews

--- trunk/Trainer/Program.cs
+++ trunk/Trainer/Program.cs
@@ -11,7 +11,28 @@ namespace Trainer
         static void Main(string[] args)
         {
             HtmlDocument htmlDoc = new HtmlDocument();
-
+            htmlDoc.Load("test.htm", Encoding.UTF8);
+            var rootNode = htmlDoc.DocumentNode;
+            var marksText = rootNode.SelectNodes("//span[@class='grade-label']");
+            foreach(var mark in marksText)
+            {
+                int grade = 0;
+                switch (mark.InnerText)
+                {
+                    case "отличная модель": 
+                        grade = 5; break;
+                    case "хорошая модель":
+                        grade = 4; break;
+                    case "обычная модель":
+                        grade = 3; break;
+                    case "плохая модель":
+                        grade = 2; break;
+                    case "ужасная модель":
+                        grade = 1; break;
+                    default: break;
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
