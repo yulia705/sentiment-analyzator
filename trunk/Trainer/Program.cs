17ef9b55d74a99a0bf405c46ce630cfb99c54dc3
lesha@kalmykovs.com <lesha@kalmykovs.com@4b2eca94-256c-a624-0d39-11434cdaca9f>
2012-05-21 10:12:15 +0000

    added creating xml

--- trunk/Trainer/Program.cs
+++ trunk/Trainer/Program.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Collections;
 using HtmlAgilityPack;
+using System.Xml;
 
 namespace Trainer
 {
@@ -14,25 +16,86 @@ namespace Trainer
             htmlDoc.Load("test.htm", Encoding.UTF8);
             var rootNode = htmlDoc.DocumentNode;
             var marksText = rootNode.SelectNodes("//span[@class='grade-label']");
+            List<int> grades = new List<int>();
+            // Get marks list
             foreach(var mark in marksText)
             {
-                int grade = 0;
                 switch (mark.InnerText)
                 {
                     case "отличная модель": 
-                        grade = 5; break;
+                        grades.Add(5); break;
                     case "хорошая модель":
-                        grade = 4; break;
+                        grades.Add(4); break;
                     case "обычная модель":
-                        grade = 3; break;
+                        grades.Add(3); break;
                     case "плохая модель":
-                        grade = 2; break;
+                        grades.Add(2); break;
                     case "ужасная модель":
-                        grade = 1; break;
+                        grades.Add(1); break;
                     default: break;
                 }
             }
-            Console.ReadLine();
+            //Get texts for marks
+            List<string> advantages = new List<string>();
+            List<string> disadvantages = new List<string>();
+            List<string> comments = new List<string>();
+            var texts = rootNode.SelectNodes("//div[@class='data']");
+            foreach(var text in texts)
+            {
+                
+                if (text.ChildNodes[2].Name == "div")
+                {
+                    //Достоинства
+                    advantages.Add(text.ChildNodes[3].InnerText);
+                    //Недостатки
+                    disadvantages.Add(text.ChildNodes[4].InnerText);
+                    //Комментарий
+                    comments.Add(text.ChildNodes[5].InnerText);
+                }
+                else
+                {
+                    //Достоинства
+                    advantages.Add(text.ChildNodes[2].InnerText);
+                    //Недостатки
+                    disadvantages.Add(text.ChildNodes[3].InnerText);
+                    //Комментарий
+                    comments.Add(text.ChildNodes[4].InnerText);
+                }
+            }
+            //Generating XML
+            XmlDocument xml = new XmlDocument();
+            var xmlNode = xml.CreateNode(XmlNodeType.XmlDeclaration, "", "");
+            xml.AppendChild(xmlNode);
+            var xmlElem = xml.CreateElement("", "review", "");
+            //var xmlText = xml.CreateTextNode("test!");
+            //xmlElem.AppendChild(xmlText);
+            xml.AppendChild(xmlElem);
+            for (int i = 0; i < advantages.Count; i++)
+            {
+                var xmlAdvantages = xml.CreateElement("", "advantages", "");
+                var xmlAdvatagesText = xml.CreateTextNode(advantages[i]);
+                xmlAdvantages.AppendChild(xmlAdvatagesText);
+                xml.LastChild.AppendChild(xmlAdvantages);
+
+                var xmlDisadvantages = xml.CreateElement("", "disadvantages", "");
+                var xmlDisadvantagesText = xml.CreateTextNode(disadvantages[i]);
+                xmlDisadvantages.AppendChild(xmlDisadvantagesText);
+                xml.LastChild.AppendChild(xmlDisadvantages);
+
+                var xmlComments = xml.CreateElement("", "advantages", "");
+                var xmlCommentsText = xml.CreateTextNode(comments[i]);
+                xmlComments.AppendChild(xmlCommentsText);
+                xml.LastChild.AppendChild(xmlComments);
+
+                var xmlGrade = xml.CreateElement("","grade","");
+                var xmlGradeText = xml.CreateTextNode(grades[i].ToString());
+                xmlGrade.AppendChild(xmlGradeText);
+                xml.LastChild.AppendChild(xmlGrade);
+                
+            }
+            xml.Save("c:\\dsds.xml");
+
+                Console.ReadLine();
         }
     }
 }
